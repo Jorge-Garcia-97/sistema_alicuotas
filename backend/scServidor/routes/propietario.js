1df252a89fec5835e8dfc60aa35c8315b4588436
DilanTorres <44099867+DilanTorres@users.noreply.github.com>
2022-06-13 22:39:14 -0500

    actualizacion

--- backend/scServidor/routes/propietario.js
+++ backend/scServidor/routes/propietario.js
@@ -27,4 +27,38 @@ router.get("/propietario/", (req, res) => {
     }
   });
 
+  //Registrar nuevo propietario
+router.post("/propietario/nueva/", (req, res) => {
+  try {
+      const data = {
+        id_propietario: req.body.id_propietario,
+        nombre_propietario: req.body.nombre_propietario,
+        apellido_propietario: req.body.apellido_propietario,
+        rol_propietario: req.body.rol_propietario,
+        celular_propietario: req.body.celular_propietario,
+        correo_propietario: req.body.correo_propietario,
+        usuario_id_usuario: req.body.usuario_id_usuario,
+      };
+      const query = `INSERT INTO usuario (id_propietario, nombre_propietario, apellido_propietario, rol_propietario, celular_propietario, correo_propietario, usuario_id_usuario) VALUES ('${data.id_propietario}', '${data.nombre_propietario}', '${data.apellido_propietario}', '${data.rol_propietario}', '${data.celular_propietario}', '${data.correo_propietario}', '${data.usuario_id_usuario}')`;
+      getConnection(function (err, conn) {
+          if (err) {
+              return res.status(500).send("¡Algo ha salido mal!");
+          } else {
+              conn.query(query, function (err, row) {
+                  if (err) {
+                      return res
+                          .status(404)
+                          .send("No se ha podido realizar su petición");
+                  } else {
+                      return res.status(200).send("Ok");
+                  }
+              });
+          }
+          conn.release();
+      });
+  } catch (error) {
+      res.send("¡Error!. intente más tarde");
+  }
+});
+
   module.exports = router;
