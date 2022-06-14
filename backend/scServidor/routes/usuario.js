f87602fa83a97d996afc83cf1dac2ff29f6e6df2
DilanTorres <44099867+DilanTorres@users.noreply.github.com>
2022-06-13 20:07:48 -0500

    Modificacion

--- backend/scServidor/routes/usuario.js
+++ backend/scServidor/routes/usuario.js
@@ -24,4 +24,34 @@ router.get("/usuarios/", (req, res) => {
     }
   });
 
+  //Registrar nuevo usuario
+router.post("/usuarios/nueva/", (req, res) => {
+  try {
+      const data = {
+        idusuario: req.body.idusuario,
+        nombre_usuario: req.body.nombre_usuario,
+        contraseña_usuario: req.body.contraseña_usuario
+      };
+      const query = `INSERT INTO reservacion (idusuario, nmbre_usuario, contraseña_usuario) VALUES ('${data.idusuario}', '${data.nombre_usuario}', '${data.contraseña_usuario}')`;
+      getConnection(function (err, conn) {
+          if (err) {
+              return res.status(500).send("¡Algo ha salido mal!");
+          } else {
+              conn.query(query, function (err, row) {
+                  if (err) {
+                      return res
+                          .status(404)
+                          .send("No se ha podido realizar su petición");
+                  } else {
+                      return res.status(200).send("Ok");
+                  }
+              });
+          }
+          conn.release();
+      });
+  } catch (error) {
+      res.send("¡Error!. intente más tarde");
+  }
+});
+
   module.exports = router;
