4a6636e83239bb3e1a013ec09940d52466bbf2da
DilanTorres <44099867+DilanTorres@users.noreply.github.com>
2022-06-15 23:34:20 -0500

    cambios

--- backend/scServidor/routes/pago_alicuota.js
+++ backend/scServidor/routes/pago_alicuota.js
@@ -28,13 +28,13 @@ router.get("/pagoalicuota/", (req, res) => {
 router.post("/pagoalicuota/save/", (req, res) => {
     try {
       const data = {
-          idpago_alicuota: req.body.idpago_alicuota,
+          id_pago_alicuota: req.body.id_pago_alicuota,
           valor_alicuota: req.body.valor_alicuota,
           fecha_alicuota: req.body.fecha_alicuota,
           estado_alicuota: req.body.estado_alicuota,
           atraso_alicuota: req.body.atraso_alicuota
       };
-      const query = `INSERT INTO pago_alicuota (idpago_alicuota, valor_alicuota, fecha_alicuota, estado_alicuota, atraso_alicuota) VALUES ('${data.idpago_alicuota}', '${data.valor_alicuota}', '${data.fecha_alicuota}','${data.estado_alicuota}','${data.atraso_alicuota}')`;
+      const query = `INSERT INTO pago_alicuota (id_pago_alicuota, valor_alicuota, fecha_alicuota, estado_alicuota, atraso_alicuota) VALUES ('${data.id_pago_alicuota}', '${data.valor_alicuota}', '${data.fecha_alicuota}','${data.estado_alicuota}','${data.atraso_alicuota}')`;
       getConnection(function (err, conn) {
         if (err) {
           return res.status(500).send("¡Algo ha salido mal!");
@@ -63,7 +63,7 @@ router.post("/pagoalicuota/edit/estado/:id/", (req, res) => {
     const data = {
       estado_alicuota: req.body.estado_alicuota,
     };
-    const query = `UPDATE pago_alicuota SET estado_alicuota= '${data.estado_alicuota}' WHERE idpago_alicuota = ?`;
+    const query = `UPDATE pago_alicuota SET estado_alicuota= '${data.estado_alicuota}' WHERE id_pago_alicuota = ?`;
     getConnection(function (err, conn) {
       if (err) {
         return res.status(500).send("Oh!, something went wrong");
