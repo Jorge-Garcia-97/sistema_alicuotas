4cee8ea9af95db7670e5aa4f3354cb550d2b7839
DilanTorres <44099867+DilanTorres@users.noreply.github.com>
2022-06-16 19:32:32 -0500

    Actualizacion de servicios de propietario

--- backend/scServidor/routes/propietario.js
+++ backend/scServidor/routes/propietario.js
@@ -61,4 +61,39 @@ router.post("/propietario/save/", (req, res) => {
   }
 });
 
+  //Editar administrador
+router.post("/propietario/edit/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = {
+      nombre_propietario: req.body.nombre_propietario,
+      apellido_propietario: req.body.apellido_propietario,
+      rol_propietario: req.body.rol_propietario,
+      celular_propietario: req.body.celular_propietario,
+      correo_propietario: req.body.correo_propietario,
+    };    
+    getConnection(function (err, conn) {
+      if (err) {
+        return res.status(500).send("Oh!, something went wrong");
+      } else {
+        const query = `UPDATE propietario SET nombre_propietario = '${data.nombre_propietario}', apellido_propietario = '${data.apellido_propietario}', rol_propietario = '${data.rol_propietario}', celular_propietario = '${data.celular_propietario}', correo_propietario = '${data.correo_propietario}' WHERE id_propietario = ?`;
+        conn.query(query, [id], function (err, row) {
+          if (err) {
+            return res
+              .status(404)
+              .send("Sorry for that. The request could not be made.");
+          } else {
+            return res.status(200).send("Ok");
+          }
+        });
+      }
+      conn.release();
+    });
+  } catch (error) {
+    console.log(error);
+    res.send("Error. Please try again later.");
+  }
+});
+
+
   module.exports = router;
