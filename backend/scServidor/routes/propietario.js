41fae3fd1f65cf84bf76372795d5ee61741e3949
DilanTorres <44099867+DilanTorres@users.noreply.github.com>
2022-06-21 15:27:40 -0500

    Actualizacion en propietarios

--- backend/scServidor/routes/propietario.js
+++ backend/scServidor/routes/propietario.js
@@ -4,30 +4,30 @@ var getConnection = require("../conexion");
 
 //consultar propietario
 router.get("/propietarios/", (req, res) => {
-    try {
-      const { password } = req.params;
-      getConnection(function (err, conn) {
-        if (err) {
-          return res.status(500).send("Error ¯\_(°.°)_/¯");
-        } else {
-          const query =
-            "SELECT * FROM propietario";
-          conn.query(query, [password], function (err, row) {
-            if (err) {
-              return res.status(404).send("Disculpas, no a funcionado");
-            } else {
-              return res.send(row);
-            }
-          });
-        }
-        conn.release();
-      });
-    } catch (error) {
-      res.send("Error. Por favor intente más tarde.");
-    }
-  });
+  try {
+    const { password } = req.params;
+    getConnection(function (err, conn) {
+      if (err) {
+        return res.status(500).send("Error ¯\_(°.°)_/¯");
+      } else {
+        const query =
+          "SELECT * FROM propietario";
+        conn.query(query, [password], function (err, row) {
+          if (err) {
+            return res.status(404).send("Disculpas, no a funcionado");
+          } else {
+            return res.send(row);
+          }
+        });
+      }
+      conn.release();
+    });
+  } catch (error) {
+    res.send("Error. Por favor intente más tarde.");
+  }
+});
 
-  //Consultar propietario por ID
+//Consultar propietario por ID
 router.get("/propietario/:id", (req, res) => {
   const { id } = req.params;
   try {
@@ -75,45 +75,12 @@ router.get("/propietario/by-cedula/:cedula", (req, res) => {
 });
 
 
-  //Registrar nuevo propietario
-router.post("/propietario/save/", (req, res) => {
-  try {
-      const data = {
-        id_propietario: req.body.id_propietario,
-        cedula_propietario: req.body.cedula_propietario,
-        nombre_propietario: req.body.nombre_propietario,
-        apellido_propietario: req.body.apellido_propietario,
-        rol_propietario: req.body.rol_propietario,
-        celular_propietario: req.body.celular_propietario,
-        correo_propietario: req.body.correo_propietario,
-        usuario_id_usuario: req.body.usuario_id_usuario,
-      };
-      const query = `INSERT INTO usuario (id_propietario, cedula_propietario, nombre_propietario, apellido_propietario, rol_propietario, celular_propietario, correo_propietario, usuario_id_usuario) VALUES ('${data.cedula_propietario}', '${data.id_propietario}', '${data.nombre_propietario}', '${data.apellido_propietario}', '${data.rol_propietario}', '${data.celular_propietario}', '${data.correo_propietario}', '${data.usuario_id_usuario}')`;
-      getConnection(function (err, conn) {
-        if (err) {
-          return res.status(500).send("¡Algo ha salido mal!");
-        } else {
-          conn.query(query, function (err, row) {
-            if (err) {
-              return res.status(404).send("No se ha podido realizar su petición");
-            } else {
-              return res.status(200).send("Ok");
-            }
-          });
-        }
-        conn.release();
-      });
-    } catch (error) {
-      console.log(error);
-      res.send("Error. Please try again later.");
-    }
-  });
-
 //Registrar nuevo propietario
 router.post("/propietario/save/", (req, res) => {
   try {
     const data = {
       id_propietario: req.body.id_propietario,
+      cedula_propietario: req.body.cedula_propietario,
       nombre_propietario: req.body.nombre_propietario,
       apellido_propietario: req.body.apellido_propietario,
       rol_propietario: req.body.rol_propietario,
@@ -121,7 +88,7 @@ router.post("/propietario/save/", (req, res) => {
       correo_propietario: req.body.correo_propietario,
       usuario_id_usuario: req.body.usuario_id_usuario,
     };
-    const query = `INSERT INTO usuario (id_propietario, nombre_propietario, apellido_propietario, rol_propietario, celular_propietario, correo_propietario, usuario_id_usuario) VALUES ('${data.id_propietario}', '${data.nombre_propietario}', '${data.apellido_propietario}', '${data.rol_propietario}', '${data.celular_propietario}', '${data.correo_propietario}', '${data.usuario_id_usuario}')`;
+    const query = `INSERT INTO usuario (id_propietario, cedula_propietario, nombre_propietario, apellido_propietario, rol_propietario, celular_propietario, correo_propietario, usuario_id_usuario) VALUES ('${data.cedula_propietario}', '${data.id_propietario}', '${data.nombre_propietario}', '${data.apellido_propietario}', '${data.rol_propietario}', '${data.celular_propietario}', '${data.correo_propietario}', '${data.usuario_id_usuario}')`;
     getConnection(function (err, conn) {
       if (err) {
         return res.status(500).send("¡Algo ha salido mal!");
@@ -129,6 +96,41 @@ router.post("/propietario/save/", (req, res) => {
         conn.query(query, function (err, row) {
           if (err) {
             return res.status(404).send("No se ha podido realizar su petición");
+          } else {
+            return res.status(200).send("Ok");
+          }
+        });
+      }
+      conn.release();
+    });
+  } catch (error) {
+    console.log(error);
+    res.send("Error. Please try again later.");
+  }
+});
+
+//Editar propietario
+router.post("/administrador/edit/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = {
+      cedula_propietario: req.body.cedula_propietario,
+      nombre_propietario: req.body.nombre_propietario,
+      apellido_propietario: req.body.apellido_propietario,
+      celular_propietario: req.body.celular_propietario,
+      correo_propietario: req.body.correo_propietario,
+      usuario_id_usuario: req.body.usuario_id_usuario,
+    };
+    getConnection(function (err, conn) {
+      if (err) {
+        return res.status(500).send("Oh!, something went wrong");
+      } else {
+        const query = `UPDATE propietario SET cedula_propietario = '${data.cedula_propietario}', nombre_propietario = '${data.nombre_propietario}', apellido_propietario = '${data.apellido_propietario}', celular_propietario = '${data.celular_propietario}', correo_propietario = '${data.correo_propietario}', usuario_id_usuario = '${data.usuario_id_usuario}' WHERE id_propietario = ?`;
+        conn.query(query, [id], function (err, row) {
+          if (err) {
+            return res
+              .status(404)
+              .send("Sorry for that. The request could not be made.");
           } else {
             return res.status(200).send("Ok");
           }
