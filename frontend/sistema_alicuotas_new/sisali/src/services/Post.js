321b787c23f16f2f9f05d983c21588f59f5a3842
DilanTorres <44099867+DilanTorres@users.noreply.github.com>
2022-07-02 18:45:20 -0500

    Otra pequeña actualizacion

--- frontend/sistema_alicuotas_new/sisali/src/services/Post.js
+++ frontend/sistema_alicuotas_new/sisali/src/services/Post.js
@@ -88,3 +88,45 @@ export const savePropiedad = async (data, propietario_id) => {
     return false;
   }
 };
+
+export const saveArea = async (data, nombre_area) => {
+  try {
+    console.log(data);
+    const response = await fetch(`http://localhost:4000/area-comunal/save/`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        nombre_area: nombre_area,
+        descripcion_area: descripcion_area,
+      }),
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+export const saveImagenArea = async (data, id) => {
+  try {
+    const response = await fetch(
+      `http://localhost:4000/area-comunal/imagen/save/${id}/`,
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+    if (response.status === 200) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
